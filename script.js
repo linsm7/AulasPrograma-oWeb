03918e8291ba363103c787208268ce4db1eaa649
Matheus Lins <111510742+linsm7@users.noreply.github.com>
2023-04-24 21:32:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,73 @@
-const enviar = document.querySelector('#enter');
+// const videogame = {
+//     marca: 'Playstation',
+//     ano: 2020
+// }
 
-enviar.addEventListener("click", function(a){
+// const jogo ={
+//     marca: 'FIFA',
+//     ano: 2013,
 
-    const txnome = document.querySelector("#txnome");
-    const nome = txnome.value
+//     mostrarDados(){
+//         console.log(`A marca é ${this.marca} e o ano é ${this.ano}`)
+//     }
+// }
+// jogo.mostrarDados()
+// jogo.mostrarDados.call(videogame)
 
-    console.log(`Nome: ${nome}`)
+// Pergunta 1:
 
-});
+function hello (name, lastName){
+    console.log(`Olá ${name} ${lastName}, seja bem vindo!`);
+}
+hello('Matheus', 'Lins');
 
-enviar.addEventListener("click", function(b){
-    const txemail = document.querySelector("#txemail");
-    const email = txemail.value
+// Pergunta 2:
 
-    console.log(`Email: ${email}`)
-});
+hello = (name, lastName) => console.log(`Olá ${name} ${lastName}, seja bem vindo!`);
+hello('Matheus', 'Lins');
 
-enviar.addEventListener("click", function(c){
-    const txtelefone = document.querySelector("#txtelefone");
-    const telefone = txtelefone.value
+//Pergunta 3:
 
-    console.log(`Telefone: ${telefone}`)
-});
+function descobreIdade(nameUser, mesNascimento, anoNascimento){
+    let idade;
+    if(mesNascimento < 4){
+        idade = (2023 - anoNascimento);
+    }else{
+        idade = (2022 - anoNascimento);
+    }
+    console.log(`Olá ${nameUser}, sua idade é ${idade} anos.`);
+}
+descobreIdade('Matheus', 1, 2004);
 
-enviar.addEventListener("click", function(d){
-    const txendereco = document.querySelector("#txendereco");
-    const endereco = txendereco.value
+//Pergunta 4:
 
-    console.log(`Endereço: ${endereco}`)
-});
+const cliente = {
+    cpf:'09079828165', 
+    nome:'matheus lins', 
+    telefone:'61982613561', 
+    email:'lins@gmail.com', 
+    dataNascimento: '24/06/2004', 
+    clienteDesde: '2015'
+}
+
+const funcionario = {
+    cpf:'44456734212', 
+    nome:'afonso', 
+    telefone:'61987653433', 
+    email:'afonso@gmail.com', 
+    dataNascimento:'20/05/1980', 
+    registro:'saudhasudhu', 
+    carteiraTrabalho:'31314', 
+    pis:'ee1313',
+
+    mostrarDados(){
+        console.log(`Seus dados são: ${this.cpf}, ${this.nome}, ${this.telefone}, ${this.email}, ${this.dataNascimento}, ${this.registro}, ${this.carteiraTrabalho}, ${this.pis}`);
+    },
+    alterarDados(cpf, nome){
+        this.cpf = cpf;
+        this.nome = nome;
+       
+    }
+}
+funcionario.alterarDados.call(cliente,'45676940323', "luis");
+funcionario.mostrarDados.call(cliente);
